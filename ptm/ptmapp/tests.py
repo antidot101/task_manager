464ad38c6ab89b840c60a6a66739bcdd8fbbd61b
ant ant <antonio_ulum@list.ru>
2020-10-08 19:45:49 +0400

    tests UserCreate, AuthToken added

--- ptm/ptmapp/tests.py
+++ ptm/ptmapp/tests.py
@@ -1,3 +1,79 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class UserTestCase(APITestCase):
+    registration_url = reverse('registration')
+    user_correct = {'username': 'Billy-Jean', 'password': 'barbstraisand%@#'}
+    user_incorrect = {'username': 'Billy-Jean', 'password': '123456'}
+    user_blank = {'username': '', 'password': '123456'}
+
+    def register_user(self, data):
+        return self.client.post(self.registration_url, data=data)
+
+    def test_create_user_success(self):
+        response = self.register_user(data=self.user_correct)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_user_already_exist(self):
+        self.register_user(data=self.user_correct)
+        response = self.register_user(data=self.user_correct)
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+
+    def test_create_user_invalid(self):
+        response1 = self.register_user(data=self.user_incorrect)
+        response2 = self.register_user(data=None)
+        response3 = self.register_user(data=self.user_blank)
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response3.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class AuthTokenTestCase(APITestCase):
+    registration_url = reverse('registration')
+    get_token_url = reverse('get_token')
+    user_correct = {'username': 'Billy-Jean', 'password': 'barbstraisand%@#'}
+    user_incorrect = {'username': 'Michael Jackson', 'password': 'barbstraisand%@#'}
+
+    def setUp(self):
+        self.client.post(self.registration_url, data=self.user_correct)
+
+    def test_get_token_success(self):
+        response = self.client.post(self.get_token_url, data=self.user_correct)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_get_token_incorrect_data(self):
+        response1 = self.client.post(self.get_token_url, data=None)
+        response2 = self.client.post(self.get_token_url, data=self.user_incorrect)
+        self.assertEqual(response1.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response2.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+
+
+# class TaskTestCase(APITestCase):
+#     registration_url = reverse('registration')
+#     get_token_url = reverse('get_token')
+#     tasks = reverse('tasks')
+#
+#     def setUp(self):
+#         self.user = self.client.post(self.registration_url, data={'username':'Billy-Jean',
+#                                                                   'password':'barbstraisand%@#'})
+#         response = self.client.post(self.get_token_url, data={'username':'Billy-Jean',
+#                                                               'password':'barbstraisand%@#'})
+#         self.token = response.data['token']
+#         self.api_authentication()
+#
+#     def api_authentication(self):
+#         self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+#
+#     def test_tasks_list_authenticated(self):
+#         response = self.client.get(self.tasks)
+#         self.assertEqual(response.status_code, status.HTTP_200_OK)
+#
+#     def test_tasks_list_unauthenticated(self):
+#         self.client.force_authenticate(user=None)
+#         response = self.client.get(self.tasks)
+#         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
