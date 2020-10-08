349843c737c8c1a03dcd54970e26eba3b358e839
ant ant <antonio_ulum@list.ru>
2020-10-08 21:33:03 +0400

    url names added

--- ptm/ptmapp/urls.py
+++ ptm/ptmapp/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('tasks/', TaskView.as_view(), name='tasks'),
     path('tasks/<int:pk>', TaskUpdate.as_view(), name='task_update'),
     path('tasks/change-history/', TaskChangeHistoryVeiw.as_view(), name='all_tasks_history'),
-    path('tasks/change-history/<int:pk>', TaskChangeHistoryVeiw.as_view(), names='task_history'),
+    path('tasks/change-history/<int:pk>', TaskChangeHistoryVeiw.as_view(), name='task_history'),
     path('register/', UserRegisterView.as_view(), name='registration'),
     path('token-auth/', views.obtain_auth_token, name='get_token')
 ]
