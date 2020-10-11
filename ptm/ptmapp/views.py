ef3f8b4662ec8e3106af4993382fec9429dd4ca3
ant ant <antonio_ulum@list.ru>
2020-10-12 03:19:12 +0400

    ListCreateAPIView, RetrieveUpdateAPIView applied for TaskView and TaskUpdateView

--- ptm/ptmapp/views.py
+++ ptm/ptmapp/views.py
@@ -1,6 +1,6 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.generics import get_object_or_404, ListAPIView
+from rest_framework.generics import get_object_or_404, ListAPIView, ListCreateAPIView, RetrieveUpdateAPIView
 from .models import Task, TaskChangeHistory
 from .serializers import TaskSerializer, TaskChangeHistorySerializer, UserRegisterSerializer
 from django.contrib.auth.models import User
@@ -9,6 +9,7 @@ from .utils import TaskFilter
 from django.db.models import Q
 from django.contrib.auth.hashers import make_password
 import dateutil.parser as parser
+from django.http import Http404
 
 
 class WelcomeView(APIView):
@@ -20,7 +21,7 @@ class WelcomeView(APIView):
                                     "https://github.com/antidot101/task_manager/blob/master/README.md"})
 
 
-class TaskView(ListAPIView):
+class TaskView(ListCreateAPIView):
     serializer_class = TaskSerializer
     filter_backends = [DjangoFilterBackend]
     filter_class = TaskFilter
@@ -29,61 +30,61 @@ class TaskView(ListAPIView):
         queryset = Task.objects.filter(user_id=self.request.user.id)
         return queryset
 
-    # def perform_create(self, serializer):
-    #     serializer.save(user=self.request.user)
-
-    def post(self, request):
-        task = request.data
-        task['user'] = request.user.id
-        serializer = TaskSerializer(data=task)
-        if serializer.is_valid(raise_exception=True):
-            task_saved = serializer.save()
-        return Response({"success": "Task '{}' created successfully".format(task_saved.task_name),
-                         "created_task": serializer.data}, status=201)
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
-class TaskUpdate(ListAPIView):
+class TaskUpdateView(RetrieveUpdateAPIView):
     serializer_class = TaskSerializer
 
     def get_queryset(self):
         if 'pk' in self.kwargs:
-            queryset = Task.objects.filter(Q(user_id=self.request.user.id) & Q(id=self.kwargs['pk']))
-            return queryset
+            queryset = Task.objects.filter(user_id=self.request.user.id, pk=self.kwargs['pk'])
+            if not queryset:
+                raise Http404
+            else:
+                return queryset
 
-    def put(self, request, pk):
-        saved_task = get_object_or_404(Task.objects.filter(user_id=request.user.id) , pk=pk)
-        task = request.data
-        serializer = TaskSerializer(instance=saved_task, data=task, partial=True)
-        if serializer.is_valid(raise_exception=True):
+    def update(self, request, *args, **kwargs):
+        task_saved = get_object_or_404(Task.objects.filter(user_id=request.user.id),
+                                       pk=self.kwargs['pk'])
+        partial = kwargs.pop('partial', False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
 
-            # detecting changed fields
-            editable_fields = ('task_name', 'description', 'status')
-            changed_fields = ""
-            for field in editable_fields:
-                if task.get(field) is not None:
-                    if getattr(saved_task, field) != task.get(field):
-                        changed_fields += field + ", "
-            if task.get('completion_date') is not None:
-                date_saved = saved_task.completion_date
-                date_request = parser.parse(task['completion_date'])
-                if date_saved != date_request:
-                    changed_fields += 'completion_date'
-            changed_fields = changed_fields.strip(', ')
-            if changed_fields == "":
-                changed_fields = "No fields changed"
+        if getattr(instance, '_prefetched_objects_cache', None):
+            # If 'prefetch_related' has been applied to a queryset, we need to
+            # forcibly invalidate the prefetch cache on the instance.
+            instance._prefetched_objects_cache = {}
 
-            task_updated = serializer.save()
+        # detecting changed fields
+        task_request = request.data
+        editable_fields = ('task_name', 'description', 'status')
+        changed_fields = ""
+        for field in editable_fields:
+            if getattr(task_saved, field) != task_request.get(field):
+                changed_fields += field + ", "
+        if task_request.get('completion_date') is not None:
+            date_saved = task_saved.completion_date
+            date_request = parser.parse(task_request['completion_date'])
+            if date_saved != date_request:
+                changed_fields += 'completion_date'
+        changed_fields = changed_fields.strip(', ')
+        if changed_fields == "":
+            changed_fields = "No fields changed"
 
-            # preparing data for task change history
-            hist_data = task_updated.__dict__
-            hist_data['task'] = pk
-            hist_data['user'] = request.user.id
-            hist_data['changed_fields'] = changed_fields
-            serializer2 = TaskChangeHistorySerializer(data=hist_data)
-            if serializer2.is_valid(raise_exception=True):
-                serializer2.save()
-        return Response({"success": "Task '{}' updated successfully".format(task_updated.task_name),
-                         "updated_task": serializer.data})
+        # preparing data for task change history
+        hist_data = serializer.data
+        hist_data['task'] = self.kwargs['pk']
+        hist_data['user'] = request.user.id
+        hist_data['changed_fields'] = changed_fields
+        hist_serializer = TaskChangeHistorySerializer(data=hist_data)
+        hist_serializer.is_valid(raise_exception=True)
+        hist_serializer.save()
+
+        return Response(serializer.data)
 
 
 class UserRegisterView(APIView):
@@ -91,12 +92,10 @@ class UserRegisterView(APIView):
 
     def post(self, request):
         username = request.data.get('username')
-        if User.objects.filter(username=username).exists():
-            return Response({"detail": "User '{}' already exist".format(username)}, status=409)
         serializer = UserRegisterSerializer(data=request.data)
-        if serializer.is_valid(raise_exception=True):
-            password = make_password(serializer.validated_data['password'])
-            serializer.save(password = password)
+        serializer.is_valid(raise_exception=True)
+        password = make_password(serializer.validated_data['password'])
+        serializer.save(password=password)
         return Response({"success": "User '{}' created successfully".format(username)}, status=201)
 
 
