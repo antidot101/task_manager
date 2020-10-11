0136245fd4cf0c8fd32de5c3af8130d1b2b9c5f9
ant ant <antonio_ulum@list.ru>
2020-10-12 03:20:49 +0400

    view name TaskUpdateView corrected.

--- ptm/ptmapp/urls.py
+++ ptm/ptmapp/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
-from .views import TaskView, TaskUpdate, UserRegisterView, TaskChangeHistoryVeiw, WelcomeView
+from .views import TaskView, TaskUpdateView, UserRegisterView, TaskChangeHistoryVeiw, WelcomeView
 from rest_framework.authtoken import views
 
 
 urlpatterns = [
     path('', WelcomeView.as_view()),
     path('tasks/', TaskView.as_view(), name='tasks'),
-    path('tasks/<int:pk>', TaskUpdate.as_view(), name='task_update'),
+    path('tasks/<int:pk>', TaskUpdateView.as_view(), name='task_update'),
     path('tasks/change-history/', TaskChangeHistoryVeiw.as_view(), name='all_tasks_history'),
     path('tasks/change-history/<int:pk>', TaskChangeHistoryVeiw.as_view(), name='task_history'),
     path('register/', UserRegisterView.as_view(), name='registration'),
